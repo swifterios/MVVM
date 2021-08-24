d5c893ff5f3c2a645d3da742845c1fbec5cee59a
Vladislav <lordroga2@gmail.com>
2021-08-24 22:13:05 +0300

    mvvm structure

---
+++ mvvm/Model/Structs/Weather.swift
@@ -0,0 +1,10 @@
+//
+//  Weather.swift
+//  mvvm
+//
+//  Created by Владислав on 24.08.2021.
+//
+
+struct Weather {
+    
+}
---
+++ mvvm/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  mvvm
+//
+//  Created by Владислав on 24.08.2021.
+//
+
+import Foundation
