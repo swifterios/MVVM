21949a9686d7908845a47dd16197b1104d8b4f44
Vladislav <lordroga2@gmail.com>
2021-09-02 21:49:27 +0300

    created appCoordinator used appCoordinator in scene

---
+++ mvvm/AppCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  AppCoordinator.swift
+//  mvvm
+//
+//  Created by Владислав on 02.09.2021.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinator: [Coordinator] { get }
+    func start()
+}
+
+final class AppCoordinator: Coordinator {
+    private(set) var childCoordinator: [Coordinator] = []
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+}
