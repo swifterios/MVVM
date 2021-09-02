c4c3a631a4afd0a511908573961bd43eb358ade3
Vladislav <lordroga2@gmail.com>
2021-09-02 22:33:36 +0300

    added plus button added extension for colors

--- mvvm/EventListViewController.swift
+++ mvvm/EventListViewController.swift
@@ -18,6 +18,19 @@ class EventListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setupViews()
+    }
+    
+    private func setupViews() {
+        let plusImage = UIImage(systemName: "plus.circle.fill")
+        let barButtonItem = UIBarButtonItem(image: plusImage, style: .plain, target: self, action: #selector(tappedRightBarButton))
+        barButtonItem.tintColor = .primary
+        navigationItem.rightBarButtonItem = barButtonItem
+        navigationItem.title = "Events"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    @objc private func tappedRightBarButton() {
+        print("Tapped")
     }
 }
---
+++ mvvm/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  mvvm
+//
+//  Created by Владислав on 02.09.2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/255, green: 39/255, blue: 255/255, alpha: 0.6)
+}
