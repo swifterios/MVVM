a2158a01bae5f98cc610c2916cc97c9cbd673e15
Vladislav <lordroga2@gmail.com>
2021-09-05 20:38:36 +0300

    viewmodel for addEvent

--- mvvm/AddEventViewModel.swift
+++ mvvm/AddEventViewModel.swift
@@ -8,9 +8,28 @@
 import Foundation
 
 final class AddEventViewModel {
+    
+    enum Cell {
+        case titleSubtitle(TitleSubtitleCellViewModel)
+        case titleImage
+    }
+    
     var coordinator: AddEventCoordinator?
     
     func viewDidDisappear() {
         coordinator?.didFinishAddEvent()
     }
+}
+
+final class TitleSubtitleCellViewModel {
+    let title: String
+    private(set) var subtitle: String
+    let placeholder: String
+    
+    init(title: String, subtitle: String, placeholder: String) {
+        self.title = title
+        self.subtitle = subtitle
+        self.placeholder = placeholder
+    }
+    
 }
